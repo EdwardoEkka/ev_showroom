fc3a49961bfedfa2fb864adda54b36fed4513662
Vishal Ekka <vishalekka18@gmail.com>
2024-02-27 12:10:45 +0530

    Push the changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and make websites
+          Learn React and make websites for fun
         </a>
       </header>
     </div>
