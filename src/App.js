f8c8aac516d2b6125180e43dabcf3791ac62d4d7
Vishal Ekka <vishalekka18@gmail.com>
2024-02-28 01:07:40 +0530

    Card pushed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import VehicleCard from './components/v_card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and make websites for fun
-        </a>
-      </header>
+      <VehicleCard/>
     </div>
   );
 }
